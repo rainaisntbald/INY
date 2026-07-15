b9e33fcba449ffc42ff4e881de2188255f074fea
rainaisntbald <bumpjammy1@gmail.com>
2026-07-15 23:24:01 +0100

    Change to a stable paper API version

--- iny-bukkit/build.gradle.kts
+++ iny-bukkit/build.gradle.kts
@@ -14,8 +14,8 @@ dependencies {
     api(project(":iny-core"))
     bundledCore(project(":iny-core"))
 
-    compileOnly("io.papermc.paper:paper-api:26.1.1.build.+")
-    testImplementation("io.papermc.paper:paper-api:26.1.1.build.+")
+    compileOnly("io.papermc.paper:paper-api:26.1.2.build.74-stable")
+    testImplementation("io.papermc.paper:paper-api:26.1.2.build.74-stable")
     testImplementation("org.junit.jupiter:junit-jupiter:5.12.2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.12.2")
 }
